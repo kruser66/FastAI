21e4df595d8321013c8a68730eb5f0091e470902
Serg Kruglov <kruser@yandex.ru>
2025-10-19 20:23:23 +0500

    add frontend settings

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="frontend/assets"), name="assets")
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
